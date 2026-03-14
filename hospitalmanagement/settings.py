b069aeec80a32301a12add93f2fc65daa05a8b09
Master 369 <147631040+prathap929master@users.noreply.github.com>
2026-03-14 20:20:40 +0530

    prepare for render deploy

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -144,3 +144,8 @@ EMAIL_HOST_PASSWORD = 'xyz' # host email password required
 # otherwise you will get SMTPAuthenticationError at /contactus
 # this process is required because google blocks apps authentication by default
 EMAIL_RECEIVING_USER = ['to@gmail.com'] # email on which you will receive messages sent from website
+
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
